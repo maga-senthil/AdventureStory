7b54f2f145e13b4a01d81ae9020dff2fa4dbb482
maga-senthil <magalakshmi.s@gmail.com>
2016-08-11 14:51:24 -0500

    initial

---
+++ story/story/Troll.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace story
+{
+    class Troll
+    {
+        
+        string smart;
+        string strong;
+        string money;
+        string dollars;
+        string fast;
+        string headstart;
+        string option;
+        
+
+
+       
+
+        public void getInput()
+        {
+            Console.WriteLine("You're walking through the forest, minding your own business, and you run into a troll! Do you FIGHT him, PAY him, or RUN?");
+            option = (Console.ReadLine().ToLower());
+        }
+        public void fightCase()
+        {
+            Console.WriteLine("how courageous!Are you strong(YES or NO) ?");
+            strong = Console.ReadLine();
+            Console.WriteLine("Are you smart");
+            smart = Console.ReadLine();
+            Answer fightAnswer = new Answer(strong, smart);
+            fightAnswer.checkAnswer();
+            
+        }
+
+        public void payCase()
+        {
+            Console.WriteLine("All right, we'll pay the troll. Do you have any money (YES or NO)?");
+            money = Console.ReadLine();
+            Console.WriteLine("Is your money in Troll Dollars ? ");
+            dollars = Console.ReadLine();
+            Answer payAnswer = new Answer(money, dollars);
+            payAnswer.checkPay();
+        }
+
+        public void runCase()
+        {
+            Console.WriteLine("Let's book it! Are you fast (YES or NO)?");
+            fast = Console.ReadLine();
+            Console.WriteLine("Did you get a head start?");
+            headstart = Console.ReadLine();
+            Answer runAnswer = new Answer(fast, headstart);
+            runAnswer.checkRun();
+        }
+        public void checkCase()
+
+        {
+            switch(this.option)
+            {
+                case "fight":
+                    fightCase();
+                    break;
+                case "pay":
+                    payCase();
+                    break;
+                case "run":
+                    runCase();
+                    break;
+                default:
+                    Console.WriteLine("I didn't understand your choice. Hit Run and try again, this time picking FIGHT, PAY, or RUN!");
+                    break;
+
+
+            }
+            
+        }
+        
+
+
+
+    }
+}
---
+++ story/story/answer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace story
+{
+    class Answer
+    {
+        string answer1;
+        string answer2;
+
+        public Answer(string answer1,string answer2)
+        {
+            this.answer1 = answer1;
+            this.answer2 = answer2;
+        }
+        public void checkAnswer()
+        {
+            if (this.answer1  == "yes" || this.answer2  == "yes")
+            {
+                Console.WriteLine("You only need one of the two! You beat the troll--nice work!");
+
+            }
+            else
+            {
+                Console.WriteLine("You're not strong OR smart? Well, if you were smarter, you probably wouldn't have tried to fight a troll. You lose!");
+
+            }
+            
+        }
+        public void checkPay()
+        {
+            if (this.answer1  == "yes" && this.answer2  == "yes")
+            {
+                Console.WriteLine("Great! You pay the troll and continue on your merry way.");
+
+            }
+            else
+            {
+                Console.WriteLine("Dang! This troll only takes Troll Dollars. You get whomped!");
+
+            }
+        }
+        public void checkRun()
+        {
+            if (this.answer1  == "yes" || this.answer2  == "yes")
+            {
+                Console.WriteLine("You got away--barely! You live to stroll through the forest another day.");
+            }
+            else
+            {
+                Console.WriteLine("You're not fast and you didn't get a head start? You never had a chance! The troll eats you.");
+
+            }
+
+        }
+
+
+
+
+    }
+}
